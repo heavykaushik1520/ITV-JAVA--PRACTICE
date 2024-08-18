8ddf54f160fe82c8835e4868cc1cb1cf657f56cb
heavykaushik1520 <kaushikmandavkar6@gmail.com>
2024-08-18 23:16:29 +0530

    solving logical errors

--- Frequency.java
+++ Frequency.java
@@ -4,44 +4,28 @@ public class Frequency {
     {
         int arr[] = {1, 2, 8, 3, 2, 2, 2, 5, 1};
 
-        // array frequencies will store frequency of element
-//        int length = arr.length;
-
         int fr[] = new int[arr.length];
-
         int visited = -1;
-        int i;
-        int count = 1;
 
-        for(i = 0 ; i < arr.length ; i++)
-        {
-
-
-            for(int j = i+1 ; j < arr.length ; j++)
-            {
-                if(arr[i] == arr[j])
-                {
+        for(int i = 0; i < arr.length; i++) {
+            int count = 1;
+            for(int j = i+1; j < arr.length; j++) {
+                if(arr[i] == arr[j]) {
                     count++;
-                    fr[j] = visited;
+                    fr[j] = visited; // Mark as visited
                 }
             }
-        }
-
-        if(fr[i] != visited)
-        {
-            fr[i] = count;
-        }
-
-        System.out.println("Elements Frequency : ");
-
-        for(int k = 0 ; k < fr.length ; k++)
-        {
-            if (fr[i] != visited)
-            {
-                System.out.println(" " + arr[i] + " " + fr[k]);
+            if(fr[i] != visited) {
+                fr[i] = count; // Store frequency
             }
         }
 
+        // Displaying the frequency of each element
+        System.out.println("Element | Frequency");
+        for(int k = 0; k < fr.length; k++) {
+            if(fr[k] != visited) {
+                System.out.println("   " + arr[k] + "    |    " + fr[k]);
+            }
+        }
     }
-
 }
