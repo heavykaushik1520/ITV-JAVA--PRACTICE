68802337a4d6383ae27aca5551c377feb5f665c5
heavykaushik1520 <kaushikmandavkar6@gmail.com>
2024-09-05 23:20:25 +0530

    adding butterfly pattern

---
+++ ButterFlyPattern2.java
@@ -0,0 +1,2 @@
+public class ButterFlyPattern2 {
+}
--- Pattern2.java
+++ Pattern2.java
@@ -2,9 +2,9 @@ public class Pattern2 {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 5 ; i++) {
+        for (int i = 1; i <= 5; i++) {
 
-            for (int j = 1; j <= i ; j++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
